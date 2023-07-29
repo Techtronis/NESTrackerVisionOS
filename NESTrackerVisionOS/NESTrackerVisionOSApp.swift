f161e5232d1a174031d3bb60b636d365c40d0ddc
Techis <alexisjost20@gmail.com>
2023-07-30 00:08:49 +0200

    Initial Commit

---
+++ NESTrackerVisionOS/NESTrackerVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NESTrackerVisionOSApp.swift
+//  NESTrackerVisionOS
+//
+//  Created by Alexis Jost on 30.07.23.
+//
+
+import SwiftUI
+
+@main
+struct NESTrackerVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
